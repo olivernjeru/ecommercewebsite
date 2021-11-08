fc6b24bb3a176a027cd7d2bdf3683cd16885ddcc
Oliver Njiru <olivernjiru@gmail.com>
2021-11-08 09:34:18 +0300

    second commit

---
+++ ecommercewebsite_vue/src/views/Search.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="page-search">
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import ProductBox from  '@/components/ProductBox.vue'
+export default {
+    name: 'Search'
+}
+</script>
