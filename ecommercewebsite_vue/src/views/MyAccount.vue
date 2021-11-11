58fa2276246064124d7c8c13ceb4db4396ee5614
Oliver Njiru <olivernjiru@gmail.com>
2021-11-11 16:28:54 +0300

    make MyOrders visible on my account page

--- ecommercewebsite_vue/src/views/MyAccount.vue
+++ ecommercewebsite_vue/src/views/MyAccount.vue
@@ -8,14 +8,40 @@
             <div class="column is-12">
                 <button @click="logout()" class="button is-danger">Log Out</button>
             </div>
+
+            <hr>
+
+            <div class="column is-12">
+                <h2 class="subtitle">My Orders</h2>
+
+                <OrderSummary
+                    v-for="order in orders"
+                    v-bind:key="order.id"
+                    v-bind:order="order" />
+            </div>
         </div>
     </div>    
 </template>
 
 <script>
 import axios from 'axios'
+
+import OrderSummary from '@/components/OrderSummary.vue'
 export default {
     name: 'MyAccount',
+    components: {
+        OrderSummary
+    },
+    data() {
+        return {
+            orders: []
+        }
+    },
+    mounted() {
+        document.title = 'My account | Timeless Vybe'
+
+        this.getMyOrders()
+    },
     methods: {
         logout() {
             axios.defaults.headers.common["Authorization"] = ""
@@ -27,6 +53,20 @@ export default {
             this.$store.commit('removeToken')
             
             this.$router.push('/')
+        },
+        async getMyOrders() {
+            this.$store.commit('setIsLoading', true)
+            
+            await axios
+                .get('api/v1/orders/')
+                .then(response => {
+                    this.orders = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+
+            this.$store.commit('setIsLoading', false)
         }
     }
 }
